73514087a088c5430aefcbf4adfc280d2d0676b4
misteryeo <andy.yeo@gmail.com>
2017-02-13 19:13:15 -0800

    Adding passing solution for blinky dancer call step once per second

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,9 +22,8 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
-
+      clock.tick(timeBetweenSteps); // ? It seems we added an extra tick already...
+      
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -16,10 +16,9 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-  setTimeout(this.step, this.timeBetweenSteps);
+  makeDancer.prototype.step.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-  console.log('hello', this.$node);
   this.$node.toggle();
 };
